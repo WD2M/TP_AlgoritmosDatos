835fbe7896b553c857c80fc45a0695b04a2e308b
kaleteban22ad <kaleteban@gmail.com>
2024-04-24 20:34:23 -0500

    actualizar marca con las marcas

--- TP_Algoritmos/Marca.h
+++ TP_Algoritmos/Marca.h
@@ -1,16 +1,79 @@
 #pragma once
 #include "Vestimenta.h"
 #include <string>
+#include <iostream>
 using namespace std;
 
 class Marca : Vestimenta
 {
 private:
 	string nombreMarca;
-	string estilo;
+	int N;
 	bool disponibilidad;
 public:
-	Marca();
-	~Marca();
-};
+	Marca(genero* g)
 
+	{
+		N = 1 + rand() % (6);
+		if (g->getgenero() == "VestimentaHombre")
+		{
+			if (N == 1)
+			{
+				nombreMarca = "Shein";
+			}
+			if (N == 2)
+			{
+				nombreMarca = "Romwe";
+			}
+			if (N == 3)
+			{
+				nombreMarca = "Zaful";
+			}
+			if (N == 4)
+			{
+				nombreMarca = "Fashion Nova";
+			}
+			if (N == 5)
+			{
+				nombreMarca = "H&M";
+			}
+			if (N == 6)
+			{
+				nombreMarca = "Forever 21";
+			}
+		}
+		if (g->getgenero() == "VestimentaMujer")
+		{
+			if (N == 1)
+			{
+				nombreMarca = "Shein";
+			}
+			if (N == 2)
+			{
+				nombreMarca = "Romwe";
+			}
+			if (N == 3)
+			{
+				nombreMarca = "Zaful";
+			}
+			if (N == 4)
+			{
+				nombreMarca = "Fashion Nova";
+			}
+			if (N == 5)
+			{
+				nombreMarca = "H&M";
+			}
+			if (N == 6)
+			{
+				nombreMarca = "Forever 21";
+			}
+		}
+	}
+	~Marca();
+	string getmarca()
+	{
+		return Marca;
+	}
+
+};
