6dbcd7049fd3f9cd4600d13242cda151eb82f553
kaleteban22ad <kaleteban@gmail.com>
2024-04-28 21:58:27 -0500

    compra

--- TP_Algoritmos/Comprar.cpp
+++ TP_Algoritmos/Comprar.cpp
@@ -60,30 +60,8 @@ void  Compras::MostrarCompra()
 
 void Compras::Aumentarprenda()
 {
-	int Prenda;
-	cout << "QUE PRENDA DESEA AGREGAR: ( 1:ad 2:da 3:Dad) ";
-	cin >> Prenda;
-	switch (Prenda)
-	{
-	case 1:
-	{
-		cout << "´Prenda ad Agregada...";
-		break;
-	}
-	case 2:
-	{
-		cout << "´Prenda ad Agregada...";
-		break;
-	}
-	case 3:
-	{
-		cout << "´Prenda Dad Agregada...";
-		break;
-	}
-
-	default:
-		break;
-	}
+	int NuevaPrenda;
+	CarritoCompras::agregar();
 }
 
 void Compras::EliminarPrenda()
